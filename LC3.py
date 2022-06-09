f9f4f160258acf56ce065f72bf91faf5f2b3bd70
AnasMations <anas.ahassan@yahoo.com>
2022-06-09 16:30:01 +0200

    simulate all function

--- LC3.py
+++ LC3.py
@@ -1,18 +1,38 @@
-#TODO fill all LC3 commands with their prespective binary
-instrBinary = {
-    "ADD":"1110",
-    "AND":"0000"}
+# TODO
+# ADD done
+# AND done
+# NOT done
+# BRZ
+# BRP
+# BRN
+# JMP
+# LD
+# ST
 
-#We have 8 registers
-reg = {"R0":0, "R1":0, "R2":0, "R3":0, "R4":0, "R5":0, "R6":0, "R7":0}
+# TODO fill all LC3 commands with their prespective binary
+# Equivalent binary code of each instruction
+instrBinary = {
+    "ADD": "1110",
+    "AND": "0000"}
+
+# We have 8 registers
+REG = {"R0": 0, "R1": 0, "R2": 0, "R3": 0, "R4": 0, "R5": 0, "R6": 0, "R7": 0}
+
+# Program counter
+PC = 0
+
+# file path that contains assembly code
+FILEPATH = "test.txt"
+
 
 def decode(line):
-    return line.replace(",", "").upper().split()
+    return line.replace(",", "").replace("\n", "").upper().split()
 
-#TODO print error messages if input isn't correct
+# TODO print error messages if input isn't correct
 def errorHandling(list):
     list = decode(list)
 
+# TODO save converted binary code in a separate txt file
 def machineCode(list):
     list = decode(list)
     binary = ""
@@ -20,38 +40,53 @@ def machineCode(list):
         if instr in instrBinary:
             binary = binary + instrBinary[instr]
 
-    return  binary
+    return binary
+
 
 def simulate(list):
     list = decode(list)
+    print(list)
+    global PC
+    PC += 1
     for instr in list:
         if instr == "ADD":
             if list[3][0] == "#":
-                reg[list[1]] = reg[list[2]] + int(list[3][1:])
+                REG[list[1]] = REG[list[2]] + int(list[3][1:])
             else:
-                reg[list[1]] = reg[list[2]] + reg[list[3]]
+                REG[list[1]] = REG[list[2]] + REG[list[3]]
 
         elif instr == "AND":
             if list[3][0] == "#":
-                reg[list[1]] = reg[list[2]] & int(list[3][1:])
+                REG[list[1]] = REG[list[2]] & int(list[3][1:])
             else:
-                reg[list[1]] = reg[list[2]] & reg[list[3]]
+                REG[list[1]] = REG[list[2]] & REG[list[3]]
 
-        elif instr == "OR":
-            if list[3][0] == "#":
-                reg[list[1]] = reg[list[2]] | int(list[3][1:])
+        elif instr == "NOT":
+            if list[2][0] == "#":
+                REG[list[1]] = ~int(list[2][1:])
             else:
-                reg[list[1]] = reg[list[2]] | reg[list[3]]
+                REG[list[1]] = ~REG[list[2]]
+
+
+def simulateAll(lines):
+    for line in lines:
+        simulate(line)
+
 
 def printSimulation():
-    for k, v in reg.items():
-        print(k, v)
+    for k, v in REG.items():
+        print(k+":\t"+str(v))
+    print("PC:\t"+str(PC))
 
-#TODO simulate one line
-def simulateLine(list):
-    list = decode(list)
 
-s = input("line: ")
-print(machineCode(s))
-simulate(s)
+def openFileToList():
+    FILE = open(FILEPATH, "r")
+    lines = FILE.readlines()
+    lines = [line.rstrip() for line in lines]
+    return lines
+
+
+
+#print(machineCode(s))
+simulateAll(openFileToList())
 printSimulation()
