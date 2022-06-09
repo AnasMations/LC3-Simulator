05635d1ec67715f2400c9f71144a92cfff75a58f
AnasMations <anas.ahassan@yahoo.com>
2022-06-09 13:05:42 +0200

    simulation prototype

--- LC3.py
+++ LC3.py
@@ -4,20 +4,36 @@ instrBinary = {
     "AND":"0000"}
 
 #We have 8 registers
-R = [0, 0, 0, 0, 0, 0, 0, 0]
-reg = {"R0":R[0], "R1":R[1], "R2":R[2], "R3":R[3], "R4":R[4], "R5":R[5], "R6":R[6], "R7":R[7]}
+reg = {"R0":0, "R1":0, "R2":0, "R3":0, "R4":0, "R5":0, "R6":0, "R7":0}
 
 def decode(line):
     return line.replace(",", "").upper().split()
 
+#TODO print error messages if input isn't correct
+def errorHandling(list):
+    list = decode(list)
+
 def machineCode(list):
     list = decode(list)
     binary = ""
-    for i in list:
-        if i in instrBinary:
-            binary = binary + instrBinary[i]
+    for instr in list:
+        if instr in instrBinary:
+            binary = binary + instrBinary[instr]
 
     return  binary
+
+def simulate(list):
+    list = decode(list)
+    for instr in list:
+        if instr == "ADD":
+            if list[3][0] == "#":
+                reg[list[1]] = reg[list[2]] + int(list[3][1:])
+            else:
+                reg[list[1]] = reg[list[2]] + reg[list[3]]
+
+def printSimulation():
+    for k, v in reg.items():
+        print(k, v)
 
 #TODO simulate one line
 def simulateLine(list):
@@ -26,3 +42,5 @@ def simulateLine(list):
 
 s = input("line: ")
 print(machineCode(s))
+simulate(s)
+printSimulation()
